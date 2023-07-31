afba9edf41e44be80a8074ce66b6f3d742d9bb15
Владимир <127947487+eretichok@users.noreply.github.com>
2023-07-31 09:16:26 +0300

    Delete NewsPaper directory

--- NewsPaper/news/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Category, Post
-
-
-admin.site.register(Category)
-admin.site.register(Post)
